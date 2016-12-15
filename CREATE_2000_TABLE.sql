1cd36edd85d67ada85ef91baca744a6501ad925d
Tom Buckley <buckley.tom@gmail.com>
2016-12-15 14:00:25 -0800

    minor changes in style etc

--- CREATE_2000_TABLE.sql
+++ CREATE_2000_TABLE.sql
@@ -1,23 +1,20 @@
 CREATE VIEW EJ_2016.year_2000_tract_data AS
 SELECT 
-cn00s.POP_HUS_RENT50,
-cn00s.POP_LEP,
-cn00s.POP_ZVHHS,
-cn00s.SPFAM,
-cn00s.Share_LowIncome_Population,
-cn00s.Share_MinorityPopulation,
-cn00s.Pop65plus,
-acs14cn00.Asian_Pacific_Islander_2000,
-acs14cn00.Black_Alone_2000,
-acs14cn00.Hispanic_Alone_2000,
-acs14cn00.LowIncomePop_2000,
-acs14cn00.MinorityPop_2000,
-acs14cn00.Pop65plus_2000,
-acs14cn00.Pop_2000,
-acs14cn00.White_Alone_2000,
+	cn00s.GEOID,
+	cn00s.TotalPopulation,
+	cn00s.LowIncomePopulation,
+	cn00s.Minority_Population,
+	cn00s.Pop65plus,
+	cn00s.POP_HUS_RENT50,
+	cn00s.POP_LEP,
+	cn00s.POP_ZVHHS,
+	cn00s.SPFAM,
+	cn00s.Share_LowIncome_Population,
+	cn00s.Share_MinorityPopulation,
+	cn00s.Asian_Pacific_Islander,
+	cn00s.Black_Alone,
+	cn00s.Hispanic_Latino,
+	cn00s.White_Alone
 /*cn00s.Pop65plus/cn00s.TotalPopulation as 'Age65plusSOT',
 cn00s.LowIncomePopulation/cn00s.TotalPopulation as 'LwIncPop_SOT',*/
-cn00s.TotalPopulation
-From EJ_2016.CENSUS2000_EJ_SELECTEDVARIABLES as cn00s,
-EJ_2016.EJ_Select_Variables_ACS2014_Census2000_Compare as acs14cn00
-WHERE cn00s.GEOID = acs14cn00.GEOID
+From EJ_2016.CENSUS2000_EJ_SELECTEDVARIABLES as cn00s
--- CREATE_2010_TABLE.sql
+++ CREATE_2010_TABLE.sql
@@ -1,32 +1,34 @@
 CREATE VIEW EJ_2016.year_2010_tract_data AS
 SELECT 
-acs14s.TotalPopulation,
-acs14s.LowIncomePop,
-acs14s.LowIncPopSOT,
-acs14s.MinorityPopulation,
-acs14s.MinorityPopSOT,
-acs14s.PopAge65plus,
-acs14s.Age65plusSOT,
-acs14s.DisabledPop,
-acs14s.SPFAM,
-acs14s.SPFAM_SOT,
-acs14s.POP_LEP,
-acs14s.POP_LEP_SOT,
-acs14s.POP_ZVHHS,
-acs14s.POP_ZVHHS_SOT,
-acs14s.POP_HUS_RENT50,
-acs14s.POP_HUS_RENT50_SOT,
-acs14cn00.Asian_Pacific_Islander_2014,
-acs14cn00.Black_Alone_2014,
-acs14cn00.Hispanic_Alone_2014,
-acs14cn00.MinorityPopulation_ACS2014,
-acs14cn00.Pop65plus_ACS2014,
-dl.Disadvantage_Level,
-coc.COCFLAG_2017
+	acs14s.GEOID,
+	acs14s.TotalPopulation,
+	acs14s.LowIncomePop,
+	acs14s.LowIncPopSOT,
+	acs14s.MinorityPopulation,
+	acs14s.MinorityPopSOT,
+	acs14s.PopAge65plus,
+	acs14s.Age65plusSOT,
+	acs14s.DisabledPop,
+	acs14s.SPFAM,
+	acs14s.SPFAM_SOT,
+	acs14s.POP_LEP,
+	acs14s.POP_LEP_SOT,
+	acs14s.POP_ZVHHS,
+	acs14s.POP_ZVHHS_SOT,
+	acs14s.POP_HUS_RENT50,
+	acs14s.POP_HUS_RENT50_SOT,
+	acs14cn00.Asian_Pacific_Islander_2014,
+	acs14cn00.Black_Alone_2014,
+	acs14cn00.Hispanic_Alone_2014,
+	acs14cn00.MinorityPopulation_ACS2014,
+	acs14cn00.Pop65plus_ACS2014,
+	dl.Disadvantage_Level,
+	coc.COCFLAG_2017
 FROM 
-EJ_2016.EJ_Select_Variables_ACS2014_Census2000_Compare as acs14cn00,
-EJ_2016.ACS_2014_EJ_Selected_Variables as acs14s,
-EJ_2016.Disadvantage_Level as dl,
-EJ_2016.ACS_2014_ALL_COC_DATA_TRACTS as coc
-WHERE 
-1.GEOID==2.GEOID
+	EJ_2016.EJ_Select_Variables_ACS2014_Census2000_Compare AS acs14cn00 
+INNER JOIN
+	EJ_2016.ACS_2014_EJ_Selected_Variables AS acs14s ON acs14cn00.GEOID = acs14s.GEOID 
+INNER JOIN
+	EJ_2016.DISADVANTAGE_LEVEL AS dl ON acs14s.GEOID = dl.GEOID 
+INNER JOIN
+	EJ_2016.ACS_2014_ALL_COC_DATA_TRACTS AS coc ON dl.GEOID = coc.GEOID
