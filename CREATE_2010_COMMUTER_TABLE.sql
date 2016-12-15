e855595e4dcadd8834be4bd1b541c9a7ee76c61b
Tom Buckley <buckley.tom@gmail.com>
2016-12-15 15:53:11 -0800

    add commuter table

---
+++ CREATE_2010_COMMUTER_TABLE.sql
@@ -0,0 +1,15 @@
+CREATE VIEW EJ_2016.year_2010_commuter_data AS
+SELECT CountyFip, 
+TotalCommuters,
+EarlyMorning
+ShareEM, 
+TC_7_730am, 
+ST_7_730am, 
+ST_8_830am, 
+TC_8_830am, 
+TC_830_9am, 
+ST_830_9am, 
+TC_9_10am,
+ST_9_10am
+From EJ_2016.MegaregionCommuters
+WHERE CountyFip in (1,13,41,55,75,81,85,95,97)
