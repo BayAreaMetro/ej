75893ebc58a1c3e9ad6540a65758e0aefb8b012c
Tom Buckley <buckley.tom@gmail.com>
2016-12-22 18:42:57 -0800

    update single parent family and household renter data
    SPF was based on incomparable data and household renter data needed an update to its universe, changing from total population to renters. the source tables referenced in these queries were pulled from the census API using the URL's specified here: https://gist.github.com/tombuckley/e19adf224ea1fd94e19c7ebe1bfe9178

--- CREATE_2000_TABLE.sql
+++ CREATE_2000_TABLE.sql
@@ -9,15 +9,25 @@ SELECT
 	cn00s.Pop65plus as 'PopAge65plus',
 	cn00s.Pop65plus/cn00s.TotalPopulation as 'Age65plusSOT',
 	cn00s.DisabledPop,
-	cn00s.SPFAM,
-	cn00s.SPFAM/cn00s.TotalPopulation as 'SPFAM_SOT',
+	dec22upd.P017010 + dec22upd.P017016 as 'SPFAM',
+	CASE 
+		WHEN dec22upd.P017001 > 0 
+			THEN ((dec22upd.P017010 + dec22upd.P017016)/dec22upd.P017001) 
+		ELSE -9999 
+		END AS 'SPFAM_SOT', 
 	cn00s.POP_LEP,
 	cn00s.POP_LEP/cn00s.TotalPopulation as 'POP_LEP_SOT',
 	cn00s.POP_ZVHHS,
 	cn00s.POP_ZVHHS/cn00s.TotalPopulation as 'POP_ZVHHS_SOT',
 	cn00s.POP_HUS_RENT50,
+	CASE 
+		WHEN dec22upd.H069001 > 0 
+			THEN (cn00s.POP_HUS_RENT50/dec22upd.H069001) 
+		ELSE -9999 
+		END AS 'POP_HUS_RENT50_SOT', 
 	cn00s.POP_HUS_RENT50/cn00s.TotalPopulation as 'POP_HUS_RENT50_SOT',
 	cn00s.Asian_Pacific_Islander,
 	cn00s.Black_Alone,
 	cn00s.Hispanic_Latino
-From EJ_2016.CENSUS2000_EJ_SELECTEDVARIABLES_NO_ZERO_POP as cn00s
+From EJ_2016.CENSUS2000_EJ_SELECTEDVARIABLES_NO_ZERO_POP as cn00s LEFT JOIN
+	ej_2016.update_2000_sf3_dec22 AS dec22upd ON cn00s.GEOID = CONCAT (dec22upd.state, dec22upd.county, dec22upd.tract)
--- CREATE_2010_TABLE.sql
+++ CREATE_2010_TABLE.sql
@@ -9,14 +9,22 @@ SELECT
 	acs14s.PopAge65plus,
 	acs14s.Age65plusSOT,
 	acs14s.DisabledPop,
-	acs14s.SPFAM,
-	acs14s.SPFAM_SOT,
+	dec22upd.B11004_010E + dec22upd.B11004_016E as 'SPFAM',
+	CASE 
+		WHEN dec22upd.B11004_001E > 0 
+			THEN ((dec22upd.B11004_010E + dec22upd.B11004_016E)/dec22upd.B11004_001E) 
+		ELSE -9999 
+		END AS 'SPFAM_SOT', 
 	acs14s.POP_LEP,
 	acs14s.POP_LEP_SOT,
 	acs14s.POP_ZVHHS,
 	acs14s.POP_ZVHHS_SOT,
 	acs14s.POP_HUS_RENT50,
-	acs14s.POP_HUS_RENT50_SOT,
+	CASE 
+		WHEN dec22upd.B25070_001E > 0 
+			THEN (acs14s.POP_HUS_RENT50/dec22upd.B25070_001E) 
+		ELSE -9999 
+		END AS 'POP_HUS_RENT50_SOT', 
 	acs14cn00.Asian_Pacific_Islander_2014,
 	acs14cn00.Black_Alone_2014,
 	acs14cn00.Hispanic_Alone_2014,
@@ -30,3 +38,5 @@ INNER JOIN
 	EJ_2016.DISADVANTAGE_LEVEL AS dl ON acs14s.GEOID = dl.GEOID 
 INNER JOIN
 	EJ_2016.ACS_2014_ALL_COC_DATA_TRACTS AS coc ON dl.GEOID = coc.GEOID
+LEFT JOIN
+	EJ_2016.update_2014_acs_dec22 AS dec22upd ON coc.GEOID = CONCAT (dec22upd.state, dec22upd.county, dec22upd.tract)
